df7b21fd04ae5f80f166b06f99ee7f6fe50ab38c
ifhito <hito01010101@gmail.com>
2022-03-21 12:48:08 +0900

    websocketサーバーをサーバーが常時立つように変更

--- websocketServer/websocket.ts
+++ websocketServer/websocket.ts
@@ -1,51 +1,29 @@
-// import { listenAndServe } from "https://deno.land/std/http/mod.ts";
-// import {
-//   acceptWebSocket,
-//   acceptable,
-//   WebSocket,
-//   isWebSocketCloseEvent,
-// } from "https://deno.land/std/ws/mod.ts";
-const sockets = new Set<WebSocket>();
-const conn = await Deno.listen({ port: 80 });
-const httpConn = Deno.serveHttp(await conn.accept());
-const e = await httpConn.nextRequest();
-if (e) {
-  const { socket, response } = Deno.upgradeWebSocket(e.request);
-  socket.onopen = () => {
-  };
-  socket.onmessage = async (e) => {
-      const data = JSON.parse(e.data)
-      console.log(data)
-    const res = await fetch(data.url);
-    const html = await res.text();
-    console.log(data.url)
-    socket.send(JSON.stringify(html));
-  };
-  socket.onclose = () => console.log("WebSocket has been closed.");
-  socket.onerror = (e) => console.error("WebSocket error:", e);
-  e.respondWith(response);
-}
-// let sockets: WebSocket[] = new Array();
+import { serve } from "https://deno.land/std/http/server.ts";
+// const sockets = new Set<WebSocket>();
+// const conn = await Deno.listen({ port: 80 });
+// const httpConn = Deno.serveHttp(await conn.accept());
+// const e = await httpConn.nextRequest();
 
-
-// listenAndServe({ port: 3000 }, async (req) => {
-//     if (req.method === "GET" && req.url === "/") {
-//       // 通信をWebSocketにする
-//       if (acceptable(req)) {
-//         acceptWebSocket({
-//           conn: req.conn,
-//           bufReader: req.r,
-//           bufWriter: req.w,
-//           headers: req.headers,
-//         })
-//           .then((ws: WebSocket) => {
-//             // 機能部分
-//             sockets.push(ws);
-//             for await (const url of ws) {
-//                 const res = await fetch(url);
-//                 const html = await res.text();
-//                 await Promise.all(sockets.map((socket) => socket.send(html)));
-//               }
-
-//           });
-//       }}});
+serve((request) => {
+    console.log("socket connected!");
+  
+    // この`socket`変数はクライアント側のWebSocketオブジェクトと同様に扱える
+    const { socket, response } = Deno.upgradeWebSocket(request);
+  
+    socket.onopen = () => {
+    };
+    socket.onmessage = async (e) => {
+        const data = JSON.parse(e.data)
+        console.log(data)
+      const res = await fetch(data.url);
+      const html = await res.text();
+      console.log(data.url)
+      socket.send(JSON.stringify(html));
+    };
+    socket.onclose = () => console.log("WebSocket has been closed.");
+    socket.onerror = (e) => console.error("WebSocket error:", e);
+  
+    return response;
+  }, {
+    port: 80, // ポート番号80を使用（デフォルトは8000）
+  });
